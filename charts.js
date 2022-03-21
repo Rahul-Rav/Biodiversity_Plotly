37f186183b6ce027f302d2b9738f90e7e0180525
Rahul Raveendran <95504135+Rahul-Rav@users.noreply.github.com>
2022-03-21 03:10:39 -0400

    Add files via upload

--- charts.js
+++ charts.js
@@ -150,10 +150,9 @@ var gaugeData = [{
             { range: [0, 2], color: "red" },
             { range: [2, 4], color: "orange" },
             { range: [4, 6], color: "yellow" },
-            { range: [6, 8], color: "lightgreen" },
+            { range: [6, 8], color: "lightseagreen" },
             { range: [8, 10], color: "green" },
         ]
-
     }
 }];
 
